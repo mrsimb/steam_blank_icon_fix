1c9713099c3f795f561e6c7e083f93c047b3b00e
Andrew Pavlov <an.pavlov@mt-innovations.ru>
2025-04-26 16:23:49 +0300

    Update for Deno 2; Load icons directly from steam CDN; Try to get steam installation path from Windows registry if default one doesn't exist; Allow specifiying steam installation path trough --steampath argument; Temporarily remove handling of missing IconFile=... line in a shortcut; Update log messages;

--- fix.ts
+++ fix.ts
@@ -1,52 +1,105 @@
-const steamIconsPath = "C:/Program Files (x86)/Steam/steam/games";
-const searchPath = String(Deno.args[0] || ".");
+import { parseArgs } from "jsr:@std/cli/parse-args";
+import { join } from "jsr:@std/path";
 
+const DEFAULT_STEAM_PATH = "c:/program files (x86)/steam";
+
+const flags = parseArgs(Deno.args, {
+  string: ["steampath"],
+});
+
+async function getSteamPathFromRegistry() {
+  console.log("Searching in Windows registry...");
+
+  const args = ["query", "HKCU\\Software\\Valve\\Steam", "/v", "SteamPath"];
+  const process = new Deno.Command("reg", { args });
+  const { stdout } = await process.output();
+
+  const output = new TextDecoder().decode(stdout).trim();
+  const match = output.match(/SteamPath\s+REG_SZ\s+(.+)/);
+
+  if (!match) {
+    throw new Error("Couldn't find steam path in registry");
+  }
+
+  return match[1].trim();
+}
+
+async function resolveSteamIconsPath(installPath: string) {
+  const iconsPath = join(installPath, "/steam/games");
+  const { isDirectory } = await Deno.stat(iconsPath);
+  if (isDirectory) {
+    return iconsPath;
+  }
+  throw new Error(`${iconsPath} is not a directory`);
+}
+
+async function getSteamIconsPath() {
+  console.log("Searching for steam installation path...");
+  if (flags.steampath) {
+    return await resolveSteamIconsPath(flags.steampath);
+  }
+  try {
+    return await resolveSteamIconsPath(DEFAULT_STEAM_PATH);
+  } catch {
+    return await resolveSteamIconsPath(await getSteamPathFromRegistry());
+  }
+}
+
+const steamIconsPath = await getSteamIconsPath();
 console.log(`Steam icons path: "${steamIconsPath}"`);
+
+const searchPath = String(flags._ || ".");
 console.log(`Searching shortcuts in: "${searchPath}"\n`);
 
-for await (const entry of Deno.readDir(searchPath)) {
-  if (!entry.isFile || !entry.name.endsWith(".url")) {
-    continue;
-  }
+try {
+  for await (const entry of Deno.readDir(searchPath)) {
+    if (!entry.isFile || !entry.name.endsWith(".url")) {
+      continue;
+    }
 
-  const linkContent = await Deno.readTextFile(entry.name);
-  const gameId = linkContent.match(/rungameid\/(.+)/m)?.[1];
+    const linkContent = await Deno.readTextFile(join(searchPath, entry.name));
+    const appId = linkContent.match(/rungameid\/(.+)/m)?.[1];
+    if (!appId) {
+      continue;
+    }
 
-  if (!gameId) {
-    continue;
-  }
+    const iconPath = linkContent.match(/IconFile=(.+)/);
+    if (!iconPath) {
+      console.log(`✗ ${entry.name} - no icon path found`);
+      continue;
+    }
 
-  const infoHtml = await fetch(`https://steamdb.info/app/${gameId}/info/`)
-    .then((res) => res.text());
-  const iconUrl = infoHtml.match(/https.+\.ico/m)?.[0];
-  const iconName = iconUrl?.match(/(\w|\d)+.ico/)?.[0];
-  const hasIconPath = !!linkContent.match(/IconFile=.+/m);
-  const hasIconFile = iconName &&
-    await Deno.stat(steamIconsPath + "/" + iconName).catch(() => {});
-  const status = [!hasIconPath && "unlinked", !hasIconFile && "file missing"]
-    .filter(Boolean)
-    .join(", ") || "ok";
+    const iconName = iconPath[1].split("\\").pop()!;
 
-  if (!hasIconPath) {
-    const linkContentFixed = linkContent.replace(
-      /IconFile=$/m,
-      `IconFile=${steamIconsPath}/${iconName}`,
-    );
-    await Deno.writeTextFile(entry.name, linkContentFixed);
-  }
+    const hasIconFile = await Deno.stat(join(steamIconsPath, iconName))
+      .then((info) => info.isFile)
+      .catch(() => false);
 
-  if (!hasIconFile && iconUrl) {
-    const iconBuffer = await fetch(iconUrl).then((res) => res.arrayBuffer());
+    if (hasIconFile) {
+      console.log(`✓ ${entry.name}`);
+      continue;
+    }
+
+    const iconUrl =
+      `http://cdn.akamai.steamstatic.com/steamcommunity/public/images/apps/${appId}/${iconName}`;
+
+    const iconResponse = await fetch(iconUrl).catch(() => null);
+
+    if (!iconResponse?.ok) {
+      console.log(`✗ ${entry.name} - failed to fetch icon`);
+      continue;
+    }
+
+    const iconBuffer = await iconResponse.arrayBuffer();
+
     await Deno.writeFile(
-      steamIconsPath + "/" + iconName,
+      join(steamIconsPath, iconName),
       new Uint8Array(iconBuffer),
     );
+    console.log(`✓ ${entry.name} - fixed`);
   }
-
-  console.group(entry.name);
-  console.log(gameId, iconName);
-  console.log(status + "\n");
-  console.groupEnd();
+} catch {
+  console.log(`Failed to read ${searchPath}`);
 }
 
 console.log("Done!");
